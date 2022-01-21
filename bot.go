015c1128693401eaa902d836013a4c4b373e0e8b
fumiama <s862105088@gmail.com>
2022-01-21 14:31:07 +0800

    🔖  v1.2.4-beta5

--- bot.go
+++ bot.go
@@ -78,7 +78,7 @@ import (
 var (
 	contents = []string{
 		"* OneBot + go-cqhttp + ZeroBot + Golang",
-		"* Version 1.2.3g - 2021-12-13 21:22:45 +0800 CST",
+		"* Version 1.2.4g - 2021-12-13 21:22:45 +0800 CST",
 		"* Copyright © 2020 - 2021 FloatTech. All Rights Reserved.",
 		"* Project: https://github.com/FloatTech/ZeroBot-Plugin",
 	}
